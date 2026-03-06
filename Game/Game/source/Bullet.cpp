fca8af4f51b0f6d244643d0413325423d34d1911
narita <125cr0003@tokyo.amg.ac.jp>
2026-03-06 13:05:11 +0900

    Bulletクラスの動作処理の実装
    ・弾の動作関連
    変更は特になし
    
    ・弾の情報関連
    構造体にした
    
    /基本情報の要素/
    弾の種類
    発射者のキャラタイプ
    発射位置
    発射方向
    当たり判定の半径
    ダメージ量
    移動速度
    寿命
    
    /演出情報の要素/
    エフェクト名
    エフェクトのオフセット
    サウンド名

--- Game/Game/source/Bullet.cpp
+++ Game/Game/source/Bullet.cpp
@@ -1,9 +1,26 @@
 #include "Bullet.h"
 
+namespace BulletConstants
+{
+
+}
+
 Bullet::Bullet() 
 {
 	_eCharaType = CHARA_TYPE::BULLET;
 	_eBulletType = BULLET_TYPE::NONE;
+
+	_stcBulletConfig.bulletType		= BULLET_TYPE::NONE;
+	_stcBulletConfig.charType		= CHARA_TYPE::BULLET;
+	_stcBulletConfig.startPos		= VGet(0.0f, 0.0f, 0.0f);
+	_stcBulletConfig.dir			= VGet(0.0f, 0.0f, 0.0f);
+	_stcBulletConfig.radius			= 0.0f;
+	_stcBulletConfig.damage			= 0.0f;
+	_stcBulletConfig.lifeTime		= 0;
+
+	_stcEffectConfig.effectName = "";
+	_stcEffectConfig.effectOffset = VGet(0.0f, 0.0f, 0.0f);
+	_stcEffectConfig.soundName = "";
 }
 
 Bullet::~Bullet() 
@@ -23,17 +40,16 @@ bool Bullet::Terminate()
 
 bool Bullet::Process() 
 {
+	if(!IsBulletAlive()){ return false; }	// 弾の生存時間が残っていないならスキップ
+
 	// 移動処理
 	_vOldPos = _vPos;// 前フレームの位置を保存
 	_vPos = VAdd(_vPos, _vMove);// 位置更新
 
-	// 寿命チェック
-	_lifeTimer--;
-	if (_lifeTimer <= 0) {
-		return false;// リストから削除する
-	}
+	// 生存時間の減算処理
+	DecrementLifeTime();
 
-	return true;// 生存している
+	return true;
 }
 
 bool Bullet::Render()
@@ -49,7 +65,7 @@ bool Bullet::Render()
 
 void Bullet::DebugRender()
 {
-
+	
 }
 
 void Bullet::CollisionRender() 
@@ -70,4 +86,57 @@ void Bullet::Activate(VECTOR vStartPos, VECTOR vDir, float fRadius, float fSpeed
 	_eBulletType = bulletType;
 
 	_vMove = VScale(_vDir, _fMoveSpeed);// 移動量を設定
+}
+
+// 弾を有効化する
+void Bullet::ActivateBullet(const BulletConfig& bConfig, const BulletEffectConfig& bEffectConfig)
+{
+	// 弾情報の設定
+	SetBulletConfig(bConfig);
+
+	// 弾の演出関連の情報設定
+	SetEffectConfig(bEffectConfig);
+
+	// 他の情報設定
+	SetCoordinateConfig(bConfig);
+}
+
+// 弾情報の設定
+void Bullet::SetBulletConfig(const BulletConfig& config)
+{
+	_stcBulletConfig = config;	// 情報の設定
+}
+
+// 弾の演出関連の情報設定
+void Bullet::SetEffectConfig(const BulletEffectConfig& config)
+{
+	_stcEffectConfig = config;	// 情報設定
+}
+
+// 他の情報設定
+void Bullet::SetCoordinateConfig(const BulletConfig& config)
+{
+	_vPos		= config.startPos;				// 位置
+	_vOldPos	= config.startPos;				// 1フレーム前の位置
+	_vDir		= VNorm(config.dir);			// 向きの正規化
+	_vMove		= VScale(_vDir, config.speed);	// 移動量の計算
+}
+
+// 弾の生存時間の減算処理
+void Bullet::DecrementLifeTime()
+{
+	_lifeTimer--;	// 時間を減らす
+}
+
+// 弾の生存時間が残っているか
+bool Bullet::IsBulletAlive()const
+{
+	// 	// 生存時間が残っているなら
+	if(_lifeTimer > 0) 
+	{
+		return true;	// リストから削除する
+	}
+	
+	// 弾の生存時間が残っていない
+	return false;	
 }
--- Game/Game/source/Bullet.h
+++ Game/Game/source/Bullet.h
@@ -28,7 +28,7 @@ struct BulletEffectConfig
 {
 	// エフェクト
 	std::string effectName;	// エフェクト名
-	float effectScale;		// エフェクトのスケール
+	VECTOR effectOffset;	// エフェクトのオフセット
 
 	// サウンド
 	std::string soundName;	// サウンド名
@@ -52,22 +52,42 @@ public:
 	// 弾の発射処理
 	void Activate(VECTOR vStartPos, VECTOR vDir, float fRadius, float fSpeed, int lifeTime, CHARA_TYPE charaType, BULLET_TYPE bulletType);
 
+	// 弾を有効化する
+	void ActivateBullet(const BulletConfig& config, const BulletEffectConfig& effectConfig);
+
 	// 弾情報の設定
 	void SetBulletConfig(const BulletConfig& config);
+
+	// 弾の演出関連の情報設定
+	void SetEffectConfig(const BulletEffectConfig& config);
+
+	// 位置関係の情報設定
+	void SetCoordinateConfig(const BulletConfig& config);
 
 	// ゲッターセッター
 	CHARA_TYPE GetShooterType() const { return _eShooterType; }	// 弾の発射者を取得
 	BULLET_TYPE GetBulletType() const { return _eBulletType; }	// 弾の種類を取得
 
 private:
+	// 弾の生存時間の減算処理
+	void DecrementLifeTime();
+
+	// 弾の生存時間が残っているか
+	bool IsBulletAlive()const;
 
 
 protected:
-	BulletConfig		_config;		// 弾の情報構造体
-	BulletEffectConfig	_effectConfig;	// 弾の演出関係の構造体
 
-	int _lifeTimer;// 弾の寿命
-	CHARA_TYPE _eShooterType;// 誰が発射した弾か
-	BULLET_TYPE _eBulletType;// 弾の種類
+	// 弾の情報構造体
+	BulletConfig		_stcBulletConfig;		
+
+	// 弾の演出関係の構造体
+	BulletEffectConfig	_stcEffectConfig;	
+
+	CHARA_TYPE _eShooterType;	// 誰が発射した弾か
+	BULLET_TYPE _eBulletType;	// 弾の種類
+
+	int _lifeTimer;	// 弾の寿命
+
 };
 
